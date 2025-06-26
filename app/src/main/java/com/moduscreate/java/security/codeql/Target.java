4cedbd4f2618cc23916a10422c7ceabcf95893d1
Clément Hurlin <clement.hurlin@moduscreate.com>
2025-06-26 16:18:34 +0200

    Initial commit

---
+++ app/src/main/java/com/moduscreate/java/security/codeql/Target.java
@@ -0,0 +1,11 @@
+package com.moduscreate.java.security.codeql;
+
+public class Target {
+    public String sayHello() {
+        return "Hello from sayHello!";
+    }
+
+    public String secret() {
+        return "This is a secret!";
+    }
+}
